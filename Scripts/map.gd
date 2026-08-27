1bbfcabe22802ade23823fd89b0cf21eb81f9c57
Ucdel <ucdelfeth@gmail.com>
2026-08-27 14:43:12 +0300

    Added npc car and engine power curve.

---
+++ Scripts/map.gd
@@ -0,0 +1,13 @@
+extends Node3D
+
+@onready var path_follow_3d: PathFollow3D = $Path3D/PathFollow3D
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta: float) -> void:
+	path_follow_3d.progress_ratio += delta * delta
+	pass
--- Scripts/vehicle_body_3d.gd
+++ Scripts/vehicle_body_3d.gd
@@ -1,14 +1,23 @@
 extends VehicleBody3D
 
-@export var MAX_STEER : float = 0.4
-@export var ENGINE_POWER : float = 100
+#Max steer angle in degrees
+@export var MAX_STEER : float = 23
+@export var STEER_SPEED : float = 0.5
 @export var BRAKE_FORCE : float = 1
 @export var CAMERA_TURN : float = 0.2
+## X axis is speed of car in km/h. Y is the engine power in that speed.
+@export var POWER_CURVE : Curve
+
+#Watch variables
+@export var speed : float = 0.0
+
 @onready var camera_pivot: Marker3D = $CameraPivot
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
-	steering = move_toward(steering, Input.get_axis("right", "left") * MAX_STEER, delta * 0.5)
-	camera_pivot.rotation.y = steering / MAX_STEER * deg_to_rad(23) * CAMERA_TURN
-	engine_force = Input.get_axis("back", "foward") * ENGINE_POWER
+	speed = linear_velocity.length() * 3.6
+	steering = move_toward(steering, Input.get_axis("right", "left") * deg_to_rad(MAX_STEER), delta * STEER_SPEED)
+	camera_pivot.rotation.y = steering / deg_to_rad(MAX_STEER) * CAMERA_TURN
+	#Speed is multiplied with 3.6 to convert it to km/h
+	engine_force = Input.get_axis("back", "foward") * POWER_CURVE.sample(linear_velocity.length() * 3.6)
 	brake = Input.get_action_strength("brake") * BRAKE_FORCE

fff6a9aa6012faa909f1bb2f264853454eff5265
MrBozkurt <emrejhj4@gmail.com>
2026-08-28 02:16:02 +0300

    Terrain3D addon

--- Scripts/map.gd
+++ Scripts/map.gd
@@ -9,5 +9,4 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
-	path_follow_3d.progress_ratio += delta * delta
 	pass

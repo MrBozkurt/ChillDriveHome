c2ead1bb7a6a788bb035409bf603118d34af9b7d
Ucdel <ucdelfeth@gmail.com>
2026-08-28 15:56:21 +0300

    Adjusted directional braking logic.

--- Scripts/vehicle_body_3d.gd
+++ Scripts/vehicle_body_3d.gd
@@ -1,12 +1,12 @@
 extends VehicleBody3D
 
 #Max steer angle in degrees
-@export var MAX_STEER : float = 23
-@export var STEER_SPEED : float = 0.5
-@export var BRAKE_FORCE : float = 1
-@export var CAMERA_TURN : float = 0.2
+@export var max_steer : float = 23
+@export var steer_speed : float = 0.5
+@export var brake_force : float = 1
+@export var camera_turn : float = 0.2
 ## X axis is speed of car in km/h. Y is the engine power in that speed.
-@export var POWER_CURVE : Curve
+@export var power_curve : Curve
 
 #Watch variables
 @export var speed : float = 0.0
@@ -14,21 +14,27 @@ extends VehicleBody3D
 
 @onready var camera_pivot: Marker3D = $CameraPivot
 
+func _ready() -> void:
+	pass
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
 	speed = linear_velocity.length() * 3.6
-	steering = move_toward(steering, Input.get_axis("right", "left") * deg_to_rad(MAX_STEER), delta * STEER_SPEED)
-	camera_pivot.rotation.y = steering / deg_to_rad(MAX_STEER) * CAMERA_TURN
+	steering = move_toward(steering, Input.get_axis("right", "left") * deg_to_rad(max_steer), delta * steer_speed)
+	camera_pivot.rotation.y = steering / deg_to_rad(max_steer) * camera_turn
 	#Speed is multiplied with 3.6 to convert it to km/h
 	var front_vector := global_basis * Vector3.RIGHT
 	angle_of_attack = linear_velocity.dot(front_vector)
 	#Directional brake logic.
 	#When opposite direction pressed it brakes instead of powering wheels.
 	if angle_of_attack >= 0:
-		engine_force = Input.get_action_strength("foward") * POWER_CURVE.sample(linear_velocity.length() * 3.6)
-		brake = Input.get_action_strength("back") * BRAKE_FORCE
-	else:
-		engine_force = Input.get_action_strength("back") * POWER_CURVE.sample(linear_velocity.length() * 3.6)
-		brake = Input.get_action_strength("foward") * BRAKE_FORCE
-	engine_force = Input.get_axis("back", "foward") * POWER_CURVE.sample(linear_velocity.length() * 3.6)
-	brake = Input.get_action_strength("brake") * BRAKE_FORCE
+		engine_force = Input.get_action_strength("foward") * power_curve.sample(linear_velocity.length() * 3.6)
+		brake = Input.get_action_strength("back") * brake_force
+	if angle_of_attack <= 0:
+		engine_force = -Input.get_action_strength("back") * power_curve.sample(linear_velocity.length() * 3.6)
+		brake = Input.get_action_strength("foward") * brake_force
+	if speed < 10:
+		engine_force = Input.get_axis("back", "foward") * power_curve.sample(linear_velocity.length() * 3.6)
+		brake = 0
+	if Input.get_action_strength("brake") > 0:
+		brake = Input.get_action_strength("brake") * brake_force * 2

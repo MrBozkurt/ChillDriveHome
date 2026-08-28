f9273f08b1217a66c56090ffa6c39c7b1eeba8e9
Ucdel <ucdelfeth@gmail.com>
2026-08-28 15:04:00 +0300

    Some additions to car

--- Scripts/map.gd
+++ Scripts/map.gd
@@ -1,7 +1,5 @@
 extends Node3D
 
-@onready var path_follow_3d: PathFollow3D = $Path3D/PathFollow3D
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
--- Scripts/vehicle_body_3d.gd
+++ Scripts/vehicle_body_3d.gd
@@ -10,6 +10,7 @@ extends VehicleBody3D
 
 #Watch variables
 @export var speed : float = 0.0
+@export var angle_of_attack : float
 
 @onready var camera_pivot: Marker3D = $CameraPivot
 
@@ -19,5 +20,7 @@ func _physics_process(delta: float) -> void:
 	steering = move_toward(steering, Input.get_axis("right", "left") * deg_to_rad(MAX_STEER), delta * STEER_SPEED)
 	camera_pivot.rotation.y = steering / deg_to_rad(MAX_STEER) * CAMERA_TURN
 	#Speed is multiplied with 3.6 to convert it to km/h
+	var front_vector := global_basis * Vector3.RIGHT
+	angle_of_attack = linear_velocity.angle_to(front_vector)
 	engine_force = Input.get_axis("back", "foward") * POWER_CURVE.sample(linear_velocity.length() * 3.6)
 	brake = Input.get_action_strength("brake") * BRAKE_FORCE
